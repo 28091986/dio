0f31f8896bbe5681e85439cf553f3e20a8865d53
36dalto <140348982+36dalto@users.noreply.github.com>
2023-09-13 18:00:05 -0300

    alterações

---
+++ assets/js/main.js
@@ -0,0 +1,84 @@
+const pokemonList = document.getElementById('pokemonList')
+const loadMoreButton = document.getElementById('loadMoreButton')
+
+const maxRecords = 151
+
+const limit = 50
+let offset = 0;
+
+
+
+function loadPokemonItens(offset, limit){
+
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+
+        const newHtml = pokemons.map((pokemon) => `
+        
+        <li class="pokemon ${pokemon.type}">
+        <span class="number"> #${pokemon.number}</span>
+        <span class="name"> ${pokemon.name}</span>
+        <div class="detail">
+            <ol class="types">
+             ${pokemon.types.map((type) => `<li class="type  ${type}">${type}</li>`).join('')}
+            </ol>
+            <img src=" ${pokemon.photo}"
+              alt=" ${pokemon.name}"  >
+
+              
+        </div>
+    
+    </li>
+        
+        `).join('')
+        pokemonList.innerHTML  += newHtml
+     
+          
+        
+            })
+          
+}
+loadPokemonItens( offset, limit)
+
+loadMoreButton.addEventListener('click',  () => {
+
+    offset  += limit
+    const qtdRecordsWithNextPage = offset + limit
+ 
+    if (qtdRecordsWithNextPage >= maxRecords) {
+        const newLimit =  maxRecords - offset
+        loadPokemonItens(offset, newLimit)
+        loadMoreButton.parentElement.removeChild(loadMoreButton)
+
+        
+    }else{
+        loadPokemonItens ( offset, limit)
+    }
+   
+})
+
+function loadPokemonItens(offset, limit){ 
+    pokeApi.getPokemons(offset, limit).then((pokemons = []) => {
+        const newHtml = pokemons.map((pokemon) => `
+            <li class="pokemon ${pokemon.type}">
+                <span class="number"> #${pokemon.number}</span>
+                <span class="name"> ${pokemon.name}</span>
+                <div class="detail">
+                    <ol class="types">
+                        ${pokemon.types.map((type) => `<li class="type  ${type}">${type}</li>`).join('')}
+                    </ol>
+                    <img id="img${pokemon.number}" src="${pokemon.photo}" alt="${pokemon.name}"  >
+                    <button id="${pokemon.number}" class="clickButton">Click  aqui</button>
+                </div>
+            </li>
+        `).join('')
+        pokemonList.innerHTML  += newHtml
+
+        const buttons = document.querySelectorAll('.clickButton');
+        buttons.forEach(button => {
+            button.addEventListener('click', () => {
+                const imgElement = document.getElementById('img' + button.id);
+                window.open(imgElement.src, '_blank'); 
+            });
+        });
+    })
+}
